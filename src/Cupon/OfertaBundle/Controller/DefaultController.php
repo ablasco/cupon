87d99a85cac732547016d249e039f8df90a0d98f
Albert Blasco <albert.blasco.c@gmail.com>
2012-05-02 00:22:18 +0200

    [ADDED] Controller and template for "oferta/portada".

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -8,8 +8,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
 
-    public function indexAction($name)
+    public function portadaAction()
     {
-        return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
+            //'ciudad' => 1,
+            'id' => 1,
+            //'fecha_publicacion' => new \DateTime('today')
+        ));
+
+        /*echo '<pre>';
+        var_dump($oferta);
+        die;*/
+
+        return $this->render(
+            'OfertaBundle:Default:portada.html.twig',
+            array('oferta' => $oferta)
+        );
     }
 }
