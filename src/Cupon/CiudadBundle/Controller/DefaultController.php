bb670ff59c826b08f78ce45ca738d24a3ca6884a
Albert Blasco <albert.blasco.c@gmail.com>
2012-05-05 12:00:19 +0200

    [ADDED] Selection of "current city".

--- src/Cupon/CiudadBundle/Controller/DefaultController.php
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Cupon\CiudadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 
 class DefaultController extends Controller
@@ -11,5 +12,28 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return $this->render('CiudadBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function cambiarAction($ciudad)
+    {
+        return new RedirectResponse($this->generateUrl(
+            'portada',
+            array(
+            'ciudad' => $ciudad
+        )));
+    }
+
+    public function listaCiudadesAction($ciudad)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $ciudades = $em->getRepository('CiudadBundle:Ciudad')->findAll();
+
+        return $this->render(
+            'CiudadBundle:Default:listaCiudades.html.twig',
+            array(
+                'ciudadActual' => $ciudad,
+                'ciudades' => $ciudades
+            )
+        );
     }
 }
