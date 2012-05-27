49da126b28e33079988b9db973e7c26d903ba5c4
Albert Blasco <albert.blasco.c@gmail.com>
2012-05-27 12:32:31 +0200

    [ADDED] Page for user profile.

--- src/Cupon/UsuarioBundle/Controller/DefaultController.php
+++ src/Cupon/UsuarioBundle/Controller/DefaultController.php
@@ -107,4 +107,46 @@ class DefaultController extends Controller
             array('formulario' => $formulario->createView())
         );
     }
+
+    public function perfilAction()
+    {
+        $usuario = $this->get('security.context')->getToken()->getUser();
+        $formulario = $this->createForm(new UsuarioType(), $usuario);
+        $peticion = $this->getRequest();
+
+        if ($peticion->getMethod() == 'POST') {
+            $passwordOriginal = $formulario->getData()->getPassword();
+
+            $formulario->bindRequest($peticion);
+
+            if ($formulario->isValid()) {
+                if (null == $usuario->getPassword()) {
+                    $usuario->setPassword($passwordOriginal);
+                } else {
+                    $encoder = $this->get('security.encoder_factory')
+                        ->getEncoder($usuario);
+                    $passwordCodificado = $encoder->encodePassword(
+                        $usuario->getPassword(),
+                        $usuario->getSalt()
+                    );
+                    $usuario->setPassword($passwordCodificado);
+                }
+
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($usuario);
+                $em->flush();
+
+                $this->get('session')->setFlash('info',
+                    'Los datos de tu perfil se han actualizado correctamente'
+                );
+
+                return $this->redirect($this->generateUrl('usuario_perfil'));
+            }
+        }
+
+        return $this->render('UsuarioBundle:Default:perfil.html.twig', array(
+            'usuario'    => $usuario,
+            'formulario' => $formulario->createView()
+        ));
+    }
 }
